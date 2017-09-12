9a7e8afad683ecabd3c9d20b216e0aede6d9bfc3
Christopher Fujino <christopherfujino@gmail.com>
2017-09-12 14:12:52 -0700

    added: fleshed out Article schema

--- api-server/models/Article.js
+++ api-server/models/Article.js
@@ -3,8 +3,12 @@ const Schema = mongoose.Schema
 //const passportLocalMongoose = require('passport-local-mongoose')
 
 const ArticleSchema = new Schema({
-  name: String,
-  password: String
+  title: String,  // read from <title></title>
+  date: Date, // date highlighted
+  url: String,
+  content: String,  // markup
+  highlights: [String], // plaintext contents of highlights
+  _user: { type: Schema.Types.ObjectId, ref: 'User' } // reference by ID
 })
 
 module.exports = mongoose.model('Article', ArticleSchema);
