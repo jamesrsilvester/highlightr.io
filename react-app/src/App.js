184a9bd4731699107a452d7076d0c37986c17676
jamesrsilvester <jamesrsilvester@gmail.com>
2017-09-13 13:33:35 -0700

    react router install. header component started

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import ArticlesContainer from './ArticlesContainer.js'
 import ShowArticle from './ShowArticle.js'
-import logo from './logo.svg';
+import Header from './components/Header.js'
 import './App.css';
 
 class App extends Component {
   render() {
     return (
     <div className='home grey1'>
+      <Header/>
       <ArticlesContainer/>
       <h1 className="center-align">Single Article View</h1>
       <ShowArticle/>
--- react-app/src/ShowArticle.js
+++ react-app/src/ShowArticle.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+import {link} from 'react-router'
 
 class SingleArticle extends Component {
   render() {
