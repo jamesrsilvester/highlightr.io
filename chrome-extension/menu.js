ab984320e02cb50430a03c375783acf11d01715f
jamesrsilvester <jamesrsilvester@gmail.com>
2017-09-15 14:19:15 -0700

    removed test option from menu

--- chrome-extension/menu.js
+++ chrome-extension/menu.js
@@ -1,14 +1,5 @@
-// Copyright (c) 2010 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
 
-// A generic onclick callback function.
-function genericOnClick(info, tab) {
-  console.log("item " + info.menuItemId + " was clicked");
-  console.log("info: " + JSON.stringify(info));
-  console.log("tab: " + JSON.stringify(tab));
-}
-
+//TODO Request Shareable link URL from payload.js
 function requestLink(info, tab) {
   console.log("Your Link is...");
   alert("Your link is...");
@@ -18,6 +9,5 @@ function requestLink(info, tab) {
 var parent = chrome.contextMenus.create({"title": "highlightr.io"});
 var child1 = chrome.contextMenus.create(
   {"title": "Copy Shareable Link", "parentId": parent, "onclick": requestLink});
-var child2 = chrome.contextMenus.create(
-  {"title": "Test", "parentId": parent, "onclick": genericOnClick});
-console.log("parent:" + parent + " child1:" + child1 + " child2:" + child2);
+// var child2 = chrome.contextMenus.create(
+//   {"title": "Test", "parentId": parent, "onclick": genericOnClick});
